d376797a76a24ad55f49aefa0b7a5cbe0acc7e97
ponyp <official.kentpinero@gmail.com>
2025-10-24 18:56:30 +0800

    .env exploit

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 // const variables
-const supabase_PROJECT_URL = 'https://ypdljfstpkyckvrkbjjh.supabase.co';
-const supabase_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlwZGxqZnN0cGt5Y2t2cmtiampoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkwNTAzMjYsImV4cCI6MjA3NDYyNjMyNn0.1Xmw28KlU4nUe7j5dLc4SHZ0hWLY92Nzv9WPLtgXHJM';
+const supabase_PROJECT_URL = process.env.VITE_SUPABASE_URL!;
+const supabase_ANON_KEY = process.env.VITE_SUPABASE_ANON_KEY!;
 
 // connect to the supabase account sa internet -> communicate to the supabase services
 export const supabase = createClient( supabase_PROJECT_URL, supabase_ANON_KEY );
